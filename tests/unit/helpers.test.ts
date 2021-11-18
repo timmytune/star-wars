c9f78405abbd39ebc15261a713d6cb4d5c5661ad
Adedoyin Yinka <timmytune002@gmail.com>
2021-11-18 07:37:39 +0100

    changes

--- tests/unit/helpers.test.ts
+++ tests/unit/helpers.test.ts
@@ -2,9 +2,13 @@ import { mocked } from 'ts-jest/utils'
 import { cm_to_feet, feet_inches } from '../../src/helpers'
 //jest.mock('../src/helpers')
 
-test('direct', () => {
+test('helpers/cm_to_feet', () => {
     let ret: feet_inches = cm_to_feet(4297)
     expect(ret.feet).toBe(141)
     expect(ret.inches).toBe(0)
+
+    ret = cm_to_feet(6044)
+    expect(ret.feet).toBe(198)
+    expect(ret.inches).toBe(4)
   })
   
